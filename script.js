40a7cbf92d52927399672fb1815b663c25f2ebcd
Ksenia Konovtseva <ksenia1229@mail.ru>
2023-10-15 01:09:20 +0300

    lesson11 done

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+'use strict';
 
+const input = document.getElementById('text');
+const square = document.getElementById('square');
+const range = document.getElementById('range');
+const circle = document.getElementById('circle');
 
+btn.addEventListener('click', () => {
+  square.style.backgroundColor = input.value;
+})
 
+document.getElementById('e_btn').style.display = 'none';
 
-
+range.addEventListener('input', () => {
+  circle.style.width = range.value + '%';
+  circle.style.height = range.value + '%';
+})
